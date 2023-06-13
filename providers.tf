e99b8ee2883025b5a401e0a673917666022a1eb1
kumaresh-jr <kumaresh_jr@hotmail.com>
2023-06-13 13:14:19 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,9 +6,9 @@ terraform {
     }
   }
     backend "azurerm" {
-    resource_group_name  = "StorageAccount-ResourceGroup"
-    storage_account_name = "abcd1234"
-    container_name       = "tfstate"
+    resource_group_name  = "wbtest-rg"
+    storage_account_name = "wbtest1234"
+    container_name       = "test-container"
     key                  = "prod.terraform.tfstate"
   }
 }
