45ea894b698fc8df22f61932b21ddd37675087af
kumaresh-jr <kumaresh_jr@hotmail.com>
2023-06-13 13:00:32 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,17 @@
-
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.60.0"
+    }
+  }
+    backend "azurerm" {
+    resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "abcd1234"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+provider "azurerm" {
+  # Configuration options
+}
