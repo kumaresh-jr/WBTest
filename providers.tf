0570bfa366d88025dba89066361a7167e9091ed9
kumaresh-jr <kumaresh_jr@hotmail.com>
2023-06-15 17:16:48 +0530

    updated variables

--- providers.tf
+++ providers.tf
@@ -11,17 +11,11 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features {}
-  subscription_id=var.ARM_SUBSCRIPTION_ID
-  client_id=var.ARM_CLIENT_ID
-  client_secret=var.ARM_CLIENT_SECRET
-  tenant_id=var.ARM_TENANT_ID
+ 
 }
 provider "azurerm" {
   # Configuration options
   features {}
   alias = "dev"
-  subscription_id=var.ARM_SUBSCRIPTION_ID
-  client_id=var.ARM_CLIENT_ID
-  client_secret=var.ARM_CLIENT_SECRET
-  tenant_id=var.ARM_TENANT_ID
+
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,1 @@
-variable "ARM_SUBSCRIPTION_ID"{
 
-}
-variable "ARM_CLIENT_ID"{
-    
-}
-variable "ARM_CLIENT_SECRET"{
-    
-}
-variable "ARM_TENANT_ID"{
-    
-}
