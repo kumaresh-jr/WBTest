f1c2db5cb82278dadd1df20fe6d558ef1346e1ee
kumaresh-jr <kumaresh_jr@hotmail.com>
2023-06-15 14:56:57 +0530

    test4

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
 
 
 resource "azurerm_resource_group" "example" {
-  count = 2
-  name  = "rg${count.index}"
+  count    = 2
+  name     = "rg${count.index}"
   location = "EastUS"
 }
 
 resource "azurerm_resource_group" "env" {
-  count = length(var.resource_group_names)
-  name = "rg-${var.resource_group_names[count.index]}"
+  count    = length(var.resource_group_names)
+  name     = "rg-${var.resource_group_names[count.index]}"
   location = "EastUS"
 }
 
 resource "azurerm_resource_group" "map" {
   for_each = var.rgmap
-  name = each.key
+  name     = each.key
   location = each.value
 }
 
 resource "azurerm_public_ip" "pip" {
-  name = "pip${count.index}"
+  name                = "pip${count.index}"
   resource_group_name = azurerm_resource_group.example[0].name
-  location = azurerm_resource_group.example[0].location
-  allocation_method = "static"
-  count = 5
+  location            = azurerm_resource_group.example[0].location
+  allocation_method   = "static"
+  count               = 5
 }
 
 
