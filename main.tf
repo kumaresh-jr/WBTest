14e259fba3e393c5767c317060d7c4c195ad8836
kumaresh-jr <kumaresh_jr@hotmail.com>
2023-06-15 12:46:59 +0530

    newtest3

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ resource "azurerm_resource_group" "example" {
   name  = "rg${count.index}"
   location = "EastUS"
 }
+
+rsource "azurerm_resource_group" "env" {
+  count = length(var.resource_group_names)
+  name = "rg-${var.resource_group_names[count.index]}"
+  location = "EastUS"
+}
 output "myrg" {
   value = azurerm_resource_group.example
 }
