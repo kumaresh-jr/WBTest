96cdca5c98b3adfe8dbf28a6e7d6bb41c57d163d
kumaresh-jr <kumaresh_jr@hotmail.com>
2023-06-15 17:12:55 +0530

    test

--- providers.tf
+++ providers.tf
@@ -5,20 +5,23 @@ terraform {
       version = "3.60.0"
     }
   }
-    /*backend "azurerm" {
-    resource_group_name  = "wbtest-rg"
-    storage_account_name = "wbtest1234"
-    container_name       = "test-container"
-    key                  = "prod.terraform.tfstate"
-  }*/
+  #required_version = "=1.4.0"
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+  subscription_id=var.ARM_SUBSCRIPTION_ID
+  client_id=var.ARM_CLIENT_ID
+  client_secret=var.ARM_CLIENT_SECRET
+  tenant_id=var.ARM_TENANT_ID
 }
 provider "azurerm" {
   # Configuration options
   features {}
- # subscription_id=var.ARM_SUBSCRIPTION_ID
- # client_id=var.ARM_CLIENT_ID
-  #client_secret=var.ARM_CLIENT_SECRET
-  #tenant_id=var.ARM_TENANT_ID
+  alias = "dev"
+  subscription_id=var.ARM_SUBSCRIPTION_ID
+  client_id=var.ARM_CLIENT_ID
+  client_secret=var.ARM_CLIENT_SECRET
+  tenant_id=var.ARM_TENANT_ID
 }
-
-
