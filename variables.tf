49bb71514ca9af3e19839987b6f23d92d85d70d9
kumaresh-jr <kumaresh_jr@hotmail.com>
2023-06-15 15:05:37 +0530

    variable update

--- variables.tf
+++ variables.tf
@@ -1,4 +1,27 @@
-variable "replication_type" {
-  default = "dev"
-  description = "dev"
+variable "nsg_rules"{
+
+  description = "a list of nsg rules"
+
+  type = list(object({
+
+    name                       = string
+
+    priority                   = number #100-4096
+
+    direction                  = string
+
+    access                     = string
+
+    protocol                   = string
+
+    source_port_range          = number
+
+    destination_port_range     = number
+
+    source_address_prefix      = string
+
+    destination_address_prefix = string
+
+  }))
+
 }
