fd8b590e355880891dd6500af9969cd119cfca63
kumaresh-jr <kumaresh_jr@hotmail.com>
2023-06-15 17:22:33 +0530

    removing public ip

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources50"
   location = "EastUS"
 }
-resource "azurerm_public_ip" "pip" {
+/*resource "azurerm_public_ip" "pip" {
   name                = "pip${count.index}"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   allocation_method   = "Static"
-  count               = 5
+  count               = 5*/
 }
 
 
