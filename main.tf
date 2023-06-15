928838248ef13f6ade6aa2d774868841abfc4bfb
kumaresh-jr <kumaresh_jr@hotmail.com>
2023-06-15 11:03:59 +0530

    test3

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "example" {
   location = "EastUS"
 }
 resource "azurerm_storage_account" "example" {
-  name                     = "yourstorageacc"
+  name                     = "storageacc202391"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
