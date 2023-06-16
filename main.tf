3d2668bbfafa59725da169eca7ab2a79eb6802c5
kumaresh-jr <kumaresh_jr@hotmail.com>
2023-06-16 10:52:00 +0530

    linux provisioners

--- main.tf
+++ main.tf
@@ -1,14 +1,85 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources50"
-  location = "EastUS"
+  name     = "example-resources"
+  location = "East US"
 }
-/*resource "azurerm_public_ip" "pip" {
-  name                = "pip${count.index}"
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+resource "azurerm_public_ip" "example" {
+  name                = "acceptanceTestPublicIp1"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   allocation_method   = "Static"
-  count               = 5
-}*/
+}
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                            = "example-machine"
+  resource_group_name             = azurerm_resource_group.example.name
+  location                        = azurerm_resource_group.example.location
+  size                            = "Standard_DS1_v2"
+  admin_username                  = "adminuser"
+  admin_password                  = "ubuntu@123"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+   provisioner "file" {
+    source      = "test.sh"
+    destination = "/tmp/test.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install nginx -y",
+      "sudo systemctl start nginx",
+      "ls /tmp/test.sh",
+      "chmod +x /tmp/test.sh",
+      "/tmp/test.sh ${var.test}"
+    ]
+  }
+ 
+    connection {
+      type     = "ssh"
+      user     = self.admin_username
+      password = self.admin_password
+      host     = self.public_ip_address    
+  }
+}
 
 
 
