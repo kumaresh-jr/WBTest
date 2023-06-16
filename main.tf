21f3dd72177a295c79827a352569bf9b99732e6c
kumaresh-jr <kumaresh_jr@hotmail.com>
2023-06-16 15:22:35 +0530

    keyvaultcreate

--- main.tf
+++ main.tf
@@ -1,86 +1,29 @@
+data "azurerm_client_config" "current" {
+}
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
+  name     = "my-vault"
+  location = "West Europe"
+}
+resource "azurerm_key_vault" "example" {
+  name                        = "myvaulthere1234"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 90
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_key_vault_access_policy" "example" {
+  key_vault_id       = azurerm_key_vault.example.id
+  tenant_id          = data.azurerm_client_config.current.tenant_id
+  object_id          = data.azurerm_client_config.current.object_id
+  secret_permissions = ["Backup", "Delete" ,"Get", "List" ,"Purge" ,"Recover" ,"Restore" ,"Set"]
 }
-
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-resource "azurerm_public_ip" "example" {
-  name                = "acceptanceTestPublicIp1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  allocation_method   = "Static"
-}
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
+  resource "azurerm_key_vault_secret" "example" {
+    name         = "secret-sauce"
+    value        = "szechuan"
+    key_vault_id = azurerm_key_vault.example.id
   }
-}
-
-resource "azurerm_linux_virtual_machine" "example" {
-  name                            = "example-machine"
-  resource_group_name             = azurerm_resource_group.example.name
-  location                        = azurerm_resource_group.example.location
-  size                            = "Standard_DS1_v2"
-  admin_username                  = "adminuser"
-  admin_password                  = "ubuntu@123"
-  disable_password_authentication = false
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-   provisioner "file" {
-    source      = "test.sh"
-    destination = "/tmp/test.sh"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update -y",
-      "sudo apt-get install nginx -y",
-      "sudo systemctl start nginx",
-      "ls /tmp/test.sh",
-      "chmod +x /tmp/test.sh",
-      "/tmp/test.sh ${var.test}"
-    ]
-  }
- 
-    connection {
-      type     = "ssh"
-      user     = self.admin_username
-      password = self.admin_password
-      host     = self.public_ip_address    
-  }
-}
-
-
-
-
